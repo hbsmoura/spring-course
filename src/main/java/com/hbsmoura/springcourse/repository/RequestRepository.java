6c9e1e07af08ff883e7f850389363388758c459c
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2022-05-27 16:58:37 -0300

    Create request and request stage repositories

---
+++ src/main/java/com/hbsmoura/springcourse/repository/RequestRepository.java
@@ -0,0 +1,19 @@
+package com.hbsmoura.springcourse.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.hbsmoura.springcourse.domain.Request;
+import com.hbsmoura.springcourse.domain.enums.RequestState;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long> {
+
+	public List<Request> findAllByOwnerId(Long id);
+	
+	@Query("UPDATE request SET state = ?2 WHERE id = ?1")
+	public Request updateStatus(Long id, RequestState state);
+}
