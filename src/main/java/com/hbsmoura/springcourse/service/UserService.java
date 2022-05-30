dc8c6bf557f6a787a02e7528ce378cf6a1f5545c
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2022-05-30 15:36:53 -0300

    Create user service and hash util

---
+++ src/main/java/com/hbsmoura/springcourse/service/UserService.java
@@ -0,0 +1,49 @@
+package com.hbsmoura.springcourse.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hbsmoura.springcourse.domain.User;
+import com.hbsmoura.springcourse.repository.UserRepository;
+import com.hbsmoura.springcourse.service.util.HashUtil;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User save(User user) {
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+		
+		return userRepository.save(user);
+	}
+	
+	public User update(User user) {
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+		
+		return userRepository.save(user);
+	}
+	
+	public User getById(Long id) {
+		Optional<User> result = userRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	public User login(String email, String password){
+		password = HashUtil.getSecureHash(password);
+		
+		Optional<User> result = userRepository.login(email, password);
+		return result.get();
+	}
+	
+}
