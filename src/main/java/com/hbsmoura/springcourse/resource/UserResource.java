db9275cc5a9025f1f64bb52daf219bc8191e5d78
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2022-05-31 09:04:23 -0300

    Add requests by id method

--- src/main/java/com/hbsmoura/springcourse/resource/UserResource.java
+++ src/main/java/com/hbsmoura/springcourse/resource/UserResource.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hbsmoura.springcourse.domain.Request;
 import com.hbsmoura.springcourse.domain.User;
 import com.hbsmoura.springcourse.dto.UserLoginDTO;
+import com.hbsmoura.springcourse.service.RequestService;
 import com.hbsmoura.springcourse.service.UserService;
 
 @RestController
@@ -23,6 +25,9 @@ public class UserResource {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private RequestService requestService;
 	
 	@PostMapping
 	public ResponseEntity<User> save(@RequestBody User user) {
@@ -37,7 +42,7 @@ public class UserResource {
 		return ResponseEntity.ok(updated);
 	}
 	
-	@GetMapping("/id")
+	@GetMapping("/{id}")
 	public ResponseEntity<User> getById(@PathVariable(name = "id") Long id) {
 		User user = userService.getById(id);
 		return ResponseEntity.ok(user);
@@ -55,9 +60,9 @@ public class UserResource {
 		return ResponseEntity.ok(logged);
 	}
 	
-	//save
-	//update
-	//getById
-	//list
-	//login
+	@GetMapping("/{id}/requests")
+	public ResponseEntity<List<Request>> requestsById(@PathVariable(name = "id") Long id){
+		List<Request> list = requestService.listByOwnerId(id);
+		return ResponseEntity.ok(list);
+	}
 }
