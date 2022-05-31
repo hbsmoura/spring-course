ad9a6062bb5b2cd843f559a3136694f0b71a92c1
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2022-05-31 09:03:41 -0300

    Create request resource

---
+++ src/main/java/com/hbsmoura/springcourse/resource/RequestResource.java
@@ -0,0 +1,50 @@
+package com.hbsmoura.springcourse.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hbsmoura.springcourse.domain.Request;
+import com.hbsmoura.springcourse.service.RequestService;
+
+@RestController
+@RequestMapping(value = "requests")
+public class RequestResource {
+
+	@Autowired
+	private RequestService requestService;
+	
+	@PostMapping
+	public ResponseEntity<Request> save(@RequestBody Request request) {
+		Request created = requestService.save(request);
+		return ResponseEntity.status(HttpStatus.CREATED).body(created);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Request> update(@PathVariable(name = "id") Long id, @RequestBody Request request) {
+		request.setId(id);
+		Request updated = requestService.save(request);
+		return ResponseEntity.ok(updated);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Request> getById(@PathVariable(name = "id") Long id) {
+		Request request = requestService.getById(id);
+		return ResponseEntity.ok(request);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Request>> list() {
+		List<Request> list = requestService.list();
+		return ResponseEntity.ok(list);
+	}
+}
