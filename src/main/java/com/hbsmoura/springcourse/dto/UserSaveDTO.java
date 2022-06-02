c6c445c7b6f011d36577aa7d8109448dd7dfa3dc
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2022-06-02 16:01:51 -0300

    Create user save DTO and update save DTO

---
+++ src/main/java/com/hbsmoura/springcourse/dto/UserSaveDTO.java
@@ -0,0 +1,40 @@
+package com.hbsmoura.springcourse.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.hbsmoura.springcourse.domain.Request;
+import com.hbsmoura.springcourse.domain.RequestStage;
+import com.hbsmoura.springcourse.domain.User;
+import com.hbsmoura.springcourse.domain.enums.Role;
+
+import lombok.Data;
+
+@Data
+public class UserSaveDTO {
+
+	@NotBlank
+	private String name;
+	
+	@Email
+	private String email;
+	
+	@Size(min = 7, max = 99, message = "Password must have between 7 and 99 characters")
+	private String password;
+	
+	@NotNull
+	private Role role;
+	
+	private List<Request> requests = new ArrayList<>();
+	private List<RequestStage> stages = new ArrayList<>();
+	
+	public User turnIntoUser() {
+		User user = new User(null, this.name, this.email, this.password, this.role, this.requests, this.stages);
+		return user;
+	}
+}
---
+++ src/main/java/com/hbsmoura/springcourse/dto/UserUpdateDTO.java
@@ -0,0 +1,35 @@
+package com.hbsmoura.springcourse.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.hbsmoura.springcourse.domain.Request;
+import com.hbsmoura.springcourse.domain.RequestStage;
+import com.hbsmoura.springcourse.domain.User;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateDTO {
+
+	@NotBlank
+	private String name;
+	
+	@Email
+	private String email;
+	
+	@Size(min = 7, max = 99, message = "Password musr gave between 7 and 99 characters")
+	private String password;
+	
+	private List<Request> requests = new ArrayList<>();
+	private List<RequestStage> stages = new ArrayList<>();
+	
+	public User turnIntoUser() {
+		User user = new User(null, this.name, this.email, this.password, null, this.requests, this.stages);
+		return user;
+	}
+}
