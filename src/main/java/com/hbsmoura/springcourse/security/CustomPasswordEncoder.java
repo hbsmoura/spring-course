fffd5605400a49350cac6d71d549532e27849953
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2022-06-22 10:50:59 -0300

    Add security config and custom password encoder
    SecurityConfig class extends a deprecated class and needs to be updated as soon as possible

---
+++ src/main/java/com/hbsmoura/springcourse/security/CustomPasswordEncoder.java
@@ -0,0 +1,23 @@
+package com.hbsmoura.springcourse.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.hbsmoura.springcourse.service.util.HashUtil;
+
+@Component
+public class CustomPasswordEncoder implements PasswordEncoder{
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		String hash = HashUtil.getSecureHash(rawPassword.toString());
+		return hash;
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		String hash = HashUtil.getSecureHash(rawPassword.toString());
+		return hash.equals(encodedPassword);
+	}
+
+}
